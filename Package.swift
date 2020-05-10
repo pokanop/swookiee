54295f0b419a0b6ef40244ed09eb3df56a1a1bee
Sahel Jalal <sahel.jalal@icloud.com>
2020-05-09 18:02:59 -0700

    Add path to Package.swift

--- Package.swift
+++ Package.swift
@@ -20,11 +20,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SWookiee",
-            dependencies: [],
-            exclude: ["Demo"]),
+            path: "Sources"),
         .testTarget(
             name: "SWookieeTests",
             dependencies: ["SWookiee"],
-            exclude: ["Demo"]),
+            path: "Tests")
     ]
 )
