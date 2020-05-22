7cc62268ca260ee7eb965c5864b3e11e426a7104
Sahel Jalal <sahel.jalal@icloud.com>
2020-05-21 22:40:00 -0700

    Move DateFormatter extension into separate file

--- Sources/SWookiee/Resource.swift
+++ Sources/SWookiee/Resource.swift
@@ -145,25 +145,3 @@ extension Resource {
     }
     
 }
-
-extension DateFormatter {
-    
-    static let dateDecoder: (Decoder) throws -> Date = { decoder in
-        // Unfortunately Swift date formatting doesn't handle all variants of ISO8601
-        // easily and we need to provide a custom date decoding strategy.
-        // See this for details: https://forums.swift.org/t/iso8601dateformatter-fails-to-parse-a-valid-iso-8601-date/22999/8
-        let container = try decoder.singleValueContainer()
-        let dateString = try container.decode(String.self)
-        
-        if #available(OSX 10.13, *), #available(iOS 11.0, *) {
-            let formatter = ISO8601DateFormatter()
-            formatter.formatOptions = [.withFractionalSeconds]
-            if let date = formatter.date(from: dateString) {
-                return date
-            }
-        }
-        
-        throw DecodingError.dataCorruptedError(in: container, debugDescription: "Date string does not match expected format.")
-    }
-    
-}
