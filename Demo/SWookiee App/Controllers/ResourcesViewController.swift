1d578161cb45837501c9ea19cbce3ec58f2ed4c9
Sahel Jalal <sahel.jalal@icloud.com>
2020-05-24 22:39:17 -0700

    Update to latest package and add boilerplate for ResourcesViewController

--- Demo/SWookiee App/Controllers/ResourcesViewController.swift
+++ Demo/SWookiee App/Controllers/ResourcesViewController.swift
@@ -10,8 +10,40 @@ import SWookiee
 
 class ResourcesViewController: UIViewController {
     
+    private enum SectionLayoutKind {
+        case main
+    }
+    
     var section: Section!
     var resources: [AnyResource] = []
+    
+    private typealias DataSource = UICollectionViewDiffableDataSource<SectionLayoutKind, AnyResource>
+    private typealias Snapshot = NSDiffableDataSourceSnapshot<SectionLayoutKind, Section>
+    
+    private lazy var datasource: DataSource = DataSource(collectionView: collectionView) { (collectionView, indexPath, item) -> UICollectionViewCell? in
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SectionCell.reuseIdentifier, for: indexPath) as? SectionCell else { return nil }
+        cell.configure(item: Section.allCases[indexPath.row])
+        return cell
+    }
+    
+    private lazy var layout: UICollectionViewCompositionalLayout = {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 12.0, leading: 16.0, bottom: 12.0, trailing: 16.0)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(120.0))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }()
+    
+    private lazy var collectionView: UICollectionView = {
+        let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        view.delegate = self
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,4 +52,10 @@ class ResourcesViewController: UIViewController {
         view.backgroundColor = .white
     }
 
+}
+
+extension ResourcesViewController: UICollectionViewDelegate {
+    
+    
+    
 }
