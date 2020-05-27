55dd200ee97c0fbc4b1e151ff8740f57c59113f2
Sahel Jalal <sahel.jalal@icloud.com>
2020-05-26 20:19:08 -0700

    Fix label spacing for keys and values in AttributeCell

--- Demo/SWookiee App/Views/AttributeCell.swift
+++ Demo/SWookiee App/Views/AttributeCell.swift
@@ -21,10 +21,12 @@ class AttributeCell: UICollectionViewCell, ReuseProvider {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = UIFont.starWarsBodyFont
-        label.lineBreakMode = .byWordWrapping
+        label.lineBreakMode = .byTruncatingTail
         label.numberOfLines = 0
         label.textColor = .darkGray
         label.textAlignment = .right
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.5
         return label
     }()
     
@@ -46,13 +48,12 @@ class AttributeCell: UICollectionViewCell, ReuseProvider {
         contentView.addSubview(valueLabel)
         
         [
-            keyLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.3),
+            keyLabel.widthAnchor.constraint(equalTo: valueLabel.widthAnchor, multiplier: 0.5),
             keyLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
             keyLabel.topAnchor.constraint(equalTo: contentView.topAnchor),
             keyLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
-            keyLabel.trailingAnchor.constraint(equalTo: valueLabel.leadingAnchor, constant: -8.0),
-            valueLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.7),
-            valueLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            keyLabel.trailingAnchor.constraint(equalTo: valueLabel.leadingAnchor, constant: -16.0),
+            valueLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16.0),
             valueLabel.topAnchor.constraint(equalTo: contentView.topAnchor),
             valueLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
         ].activate()
